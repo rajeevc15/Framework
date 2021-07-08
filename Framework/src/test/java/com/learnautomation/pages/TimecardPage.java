607d2958db6c94142e834e208c124b35bd81cece
Rajeev C <c.rajeev.menon@gmail.com>
2021-07-08 14:36:18 +0530

    new change

--- Framework/src/test/java/com/learnautomation/pages/TimecardPage.java
+++ Framework/src/test/java/com/learnautomation/pages/TimecardPage.java
@@ -1,9 +1,14 @@
 package com.learnautomation.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Sleeper;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TimecardPage {
 	
@@ -14,11 +19,14 @@ public class TimecardPage {
 	public TimecardPage(WebDriver driver) {
 		this.driver=driver;
 	}
+	//div[contains(text(),'JCP Timecard User')]
 	
 	@FindBy(how=How.XPATH,using="//div[contains(text(),'JCP Timecard User')]")
 	WebElement timecarduser;
 	
 	public void clickTimeCardUser() {
+		WebDriverWait wait=new WebDriverWait(driver, 30);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(text(),'JCP Timecard User')]")));
 		timecarduser.click();
 	}
 
